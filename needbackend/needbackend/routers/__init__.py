e797e011830f1b0b296a5c9a6352c8b9dd07f8ed
Ammar trithakoon <whatislifetellme@gmail.com>
2024-08-28 23:02:44 +0700

    Try adding admin

--- needbackend/needbackend/routers/__init__.py
+++ needbackend/needbackend/routers/__init__.py
@@ -1,10 +1,11 @@
 from . import root
 from . import users
 from . import authentication
-
+from . import admins
 
 
 def init_router(app):
     app.include_router(root.router)
     app.include_router(users.router)
     app.include_router(authentication.router)
+    app.include_router(admins.router)
