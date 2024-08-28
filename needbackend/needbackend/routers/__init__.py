7d43cd80929d0d5ad7560925a4ecfa02323a5197
Ammar trithakoon <whatislifetellme@gmail.com>
2024-08-29 00:06:52 +0700

    Add messagesrouter

--- needbackend/needbackend/routers/__init__.py
+++ needbackend/needbackend/routers/__init__.py
@@ -2,10 +2,11 @@ from . import root
 from . import users
 from . import authentication
 from . import admins
-
+from . import messages
 
 def init_router(app):
     app.include_router(root.router)
     app.include_router(users.router)
     app.include_router(authentication.router)
     app.include_router(admins.router)
+    app.include_router(messages.router)
